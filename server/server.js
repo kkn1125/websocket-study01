1f9d82559a4676005f6a9bc0ac7891ecc769b343
kkn1125 <xpfjrnseks1@naver.com>
2022-06-30 16:02:19 +0900

    fix: 전체 카카오 테마로 변경 및 다중 입력 대기 표시

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 var cors = require("cors");
+const { getHeapStatistics } = require("v8");
 
 const serverPort = 3001;
 
 const messageHistory = [];
+let userHistory = [];
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -27,16 +29,24 @@ io.on("connection", (socket) => {
     console.log(`${socket.who} 측 메세지 : ${socket.msg}`);
   });
   socket.on("chat message", (socket) => {
+    userHistory = userHistory.filter((his) => his.who !== socket.who);
     messageHistory.push(socket);
     io.emit("chat message", messageHistory);
   });
   socket.on("is writing", (socket) => {
-    io.emit("is writing", socket);
+    if (!userHistory.find((his) => his.who === socket.who)) {
+      userHistory.push(socket);
+    }
+    io.emit("is writing", userHistory);
+  });
+  socket.on("remove history", (socket) => {
+    userHistory = userHistory.filter((his) => his.who !== socket);
+    io.emit("is writing", userHistory);
   });
   socket.on("out", (socket) => {
     messageHistory.push({
-      who: 'server',
-      msg: socket+'님이 방을 나갔습니다.',
+      who: "server",
+      msg: socket + "님이 방을 나갔습니다.",
     });
     io.emit("out", messageHistory);
   });
